661e76b6c7e08d2f61c7528782da601bf8540428
Brendan Fallon <brendanc.fallon@gmail.com>
2019-09-11 14:11:04 -0400

    implements some basic server caching to avoid blowing up heroku db with redundant data

--- routes/hikes.js
+++ routes/hikes.js
@@ -3,18 +3,11 @@ const { Hikes } = require('../db')
 const cache = require('./middleware/cache')
 
 router.get('/', cache.get, async (req, res, next) => {
-    const cacheData = cache.get(req)
-    if (cacheData === null){
-        try{
-            const hikes = await Hikess.findAll()
-            cache.set(req, hikes)
-            res.send(hikes)
-        } catch(error){
-            console.log(error)
-        }
-    }
-    else {
-        res.send(cacheData)
+    try{
+        const hikes = await Hikes.findAll()
+        res.send(hikes)
+    } catch(error){
+        console.log(error)
     }
 })
 
@@ -27,7 +20,7 @@ router.get('/:id', async (req, res, next) => {
     }
 })
 
-router.post('/', async (req, res, next) => {
+router.post('/', cache.get, cache.set, async (req, res, next) => {
     try{
         const hikes = await Hikes.bulkCreate(req.body)
         res.status(200).send(hikes)
--- routes/pins.js
+++ routes/pins.js
@@ -2,19 +2,13 @@ const router = require('express').Router()
 const { Pins } = require('../db')
 const cache = require('./middleware/cache')
 
-router.get('/', async (req, res, next) => {
-    const cacheData = cache.get(req)
-    if (cacheData === null){
-        try{
-            const pins = await Pins.findAll()
-            cache.set(req, pins)
-            res.send(pins)
-        } catch(error){
-            console.log(error)
-        }
-    }
-    else {
-        res.send(cacheData)
+router.get('/', cache.get, async (req, res, next) => {
+    try{
+        const pins = await Pins.findAll()
+        cache.set(req, pins)
+        res.send(pins)
+    } catch(error){
+        console.log(error)
     }
 })
 
